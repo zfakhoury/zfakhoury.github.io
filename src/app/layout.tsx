ac5e3abc427b22214cc5f626d0f065b238f0ae97
Zouhair Fakhoury <zouhairfakhoury@icloud.com>
2026-04-06 10:35:30 +0200

    Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,28 @@ export const metadata: Metadata = {
     icon: "/memoji.png",
     apple: "/memoji.png",
   },
+  openGraph: {
+    title: "Zouhair Fakhoury",
+    description:
+      "Technical Cofounder & iOS Developer – Engineering Student at INSA Rouen-Normandie",
+    url: "https://zfakhoury.github.io",
+    siteName: "Zouhair Fakhoury",
+    images: [
+      {
+        url: "https://zfakhoury.github.io/og.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Zouhair Fakhoury",
+    description:
+      "Technical Cofounder & iOS Developer – Engineering Student at INSA Rouen-Normandie",
+    images: ["https://zfakhoury.github.io/og.png"],
+  },
 };
 
 export default function RootLayout({
